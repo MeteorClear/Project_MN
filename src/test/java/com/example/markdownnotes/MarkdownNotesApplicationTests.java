621f44ed65c56df87168de7f4f4d5b2f40ec221a
MeteorClear <tivics9973@gmail.com>
2024-09-03 16:37:20 +0900

    .

---
+++ src/test/java/com/example/markdownnotes/MarkdownNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.markdownnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
