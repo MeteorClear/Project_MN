2e026bc040e80e095e392a6eaad6af3a97183c3c
MeteorClear <tivics9973@gmail.com>
2024-09-05 20:32:00 +0900

    add: JPA Note entity

---
+++ src/main/java/com/example/markdownnotes/model/Note.java
@@ -0,0 +1,38 @@
+package com.example.markdownnotes.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Note")
+public class Note {
+	// 테이블 정의
+	// DB 스키마에 맞출것
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	@Column(nullable = false, length = 255)
+	private String title;
+	
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String content;
+	
+	@Column(name = "created_at", nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+	private LocalDateTime createdAt;
+	
+	@Column(name = "updated_at")
+	private LocalDateTime updateddAt;
+	
+	// Constructor
+	public Note() {
+		// To do
+	}
+	
+	// Getter & Setter
+	// 아직까진 필요하지 않음, 필요시 작성
+}
