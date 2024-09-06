30a37f6ab608734e31fd9aed69fc77fa31d6516b
MeteorClear <tivics9973@gmail.com>
2024-09-06 18:33:13 +0900

    feat: select query method

--- src/main/java/com/example/markdownnotes/repository/NoteRepository.java
+++ src/main/java/com/example/markdownnotes/repository/NoteRepository.java
@@ -1,10 +1,13 @@
 package com.example.markdownnotes.repository;
 
 import com.example.markdownnotes.model.Note;
+import com.example.markdownnotes.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Integer> {
-	// 기능 추가
+	// 특정 사용자가 작성한 모든 메모 목록 조회
+	List<Note> findByUser(User user);
 }
--- src/main/java/com/example/markdownnotes/repository/UserRepository.java
+++ src/main/java/com/example/markdownnotes/repository/UserRepository.java
@@ -3,8 +3,13 @@ package com.example.markdownnotes.repository;
 import com.example.markdownnotes.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-	// 기늧 추가
+	// 이메일로 사용자 조회 - 로그인 용
+	Optional<User> findByEmail(String email);
+	
+	// 닉네임으로 사용자 조회 - 미정
+	Optional<User> findByUsername(String username);
 }
