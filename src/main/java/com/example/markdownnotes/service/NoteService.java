5dd594d1d7375e0e57c447eee814c1b4d0a45463
MeteorClear <tivics9973@gmail.com>
2024-09-06 20:20:52 +0900

    add: NoteService class

---
+++ src/main/java/com/example/markdownnotes/service/NoteService.java
@@ -0,0 +1,15 @@
+package com.example.markdownnotes.service;
+
+import com.example.markdownnotes.model.User;
+import com.example.markdownnotes.model.Note;
+import com.example.markdownnotes.repository.UserRepository;
+import com.example.markdownnotes.repository.NoteRepository;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NoteService {
+
+}
