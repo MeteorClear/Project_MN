104abfdcfa47fcc8100aa8c2351d96f68ee2697c
MeteorClear <tivics9973@gmail.com>
2024-09-06 19:20:37 +0900

    add: UserService class

---
+++ src/main/java/com/example/markdownnotes/service/UserService.java
@@ -0,0 +1,12 @@
+package com.example.markdownnotes.service;
+
+import com.example.markdownnotes.model.User;
+import com.example.markdownnotes.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+	
+}
