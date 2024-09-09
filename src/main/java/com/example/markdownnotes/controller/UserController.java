8426e6fa547fe27c6058a6f75834971c559f1185
MeteorClear <tivics9973@gmail.com>
2024-09-09 17:27:20 +0900

    style: add todo comment

--- src/main/java/com/example/markdownnotes/controller/UserController.java
+++ src/main/java/com/example/markdownnotes/controller/UserController.java
@@ -7,6 +7,20 @@ import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
-
+	private final UserService userService;
+	
+	// 모든 사용자 조회
+	
+	// 사용자 Id 로 조회
+	
+	// 사용자 email 로 조회
+	
+	// 사용자 생성
+	
+	// 사용자 삭제
+	
+	// 사용자 업데이트
 }
