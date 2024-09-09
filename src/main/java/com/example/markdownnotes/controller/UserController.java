123bd6ef8e073dcfbcff5fddac3637112303d21e
MeteorClear <tivics9973@gmail.com>
2024-09-09 17:20:43 +0900

    add: create UserController

---
+++ src/main/java/com/example/markdownnotes/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.markdownnotes.controller;
+
+import com.example.markdownnotes.model.User;
+import com.example.markdownnotes.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+public class UserController {
+
+}
