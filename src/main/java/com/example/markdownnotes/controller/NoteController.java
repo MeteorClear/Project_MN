5769f01be6e295e54928897d48e470d3734c64a2
MeteorClear <tivics9973@gmail.com>
2024-09-10 15:35:04 +0900

    style: add todo comment

--- src/main/java/com/example/markdownnotes/controller/NoteController.java
+++ src/main/java/com/example/markdownnotes/controller/NoteController.java
@@ -14,4 +14,15 @@ import java.util.Optional;
 public class NoteController {
 	private final NoteService noteService;
 	
+	// 모든 메모 조회
+	
+	// 메모 id 로 조회
+	
+	// 특정 사용자의 메모 목록 조회
+	
+	// 메모 생성
+	
+	// 메모 삭제
+	
+	// 메모 업데이트
 }
