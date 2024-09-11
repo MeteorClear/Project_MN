8b0db7c89eed18e4f08d536864e15905025f9626
MeteorClear <tivics9973@gmail.com>
2024-09-11 17:05:44 +0900

    add: test class for UserService

---
+++ src/test/java/com/example/markdownnotes/service/UserServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.markdownnotes.service;
+
+import com.example.markdownnotes.model.User;
+import com.example.markdownnotes.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class UserServiceTest {
+
+}
