942bd234219f202d3f919323b5d4dba81e96932a
MeteorClear <tivics9973@gmail.com>
2024-09-13 17:23:46 +0900

    feat: test code for getAllUsers

--- src/test/java/com/example/markdownnotes/controller/UserControllerTest.java
+++ src/test/java/com/example/markdownnotes/controller/UserControllerTest.java
@@ -7,8 +7,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import java.util.Arrays;
+import java.util.Optional;
 
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
@@ -31,6 +38,22 @@ public class UserControllerTest {
 	}
 	
 	// getAllUsers
+	@Test
+	void getAllUsers_ShouldReturnUserList_WhenUsersExist() throws Exception {
+		// Given
+        when(userService.getAllUsers()).thenReturn(Arrays.asList(user));
+
+        // When
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users")
+                .contentType(MediaType.APPLICATION_JSON))
+                
+        // Then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].email", is(user.getEmail())));
+        
+        verify(userService, times(1)).getAllUsers();
+	}
 	
 	// getUserById
 	
