5745003cdb0b8e74c6d7b55bbf6a5b1111fb1954
MeteorClear <tivics9973@gmail.com>
2024-09-19 19:31:56 +0900

    fix: remove unused import

--- src/test/java/com/example/markdownnotes/controller/NoteControllerTest.java
+++ src/test/java/com/example/markdownnotes/controller/NoteControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.*;
