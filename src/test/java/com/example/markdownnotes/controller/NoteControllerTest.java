506500fd948decb70748c1feede3a35ac11080ca
MeteorClear <tivics9973@gmail.com>
2024-09-19 14:08:46 +0900

    add: create NoteControllerTest

---
+++ src/test/java/com/example/markdownnotes/controller/NoteControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.markdownnotes.controller;
+
+import com.example.markdownnotes.model.Note;
+import com.example.markdownnotes.service.NoteService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class NoteControllerTest {
+
+}
