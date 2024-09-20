a7b9e9d17a26c48d6e5682fe3f31a45a84370e8b
MeteorClear <tivics9973@gmail.com>
2024-09-20 17:23:53 +0900

    feat: SecurityConfig setup

--- src/main/java/com/example/markdownnotes/config/SecurityConfig.java
+++ src/main/java/com/example/markdownnotes/config/SecurityConfig.java
@@ -1,5 +1,18 @@
 package com.example.markdownnotes.config;
 
+import com.example.markdownnotes.security.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
+	
+	
 }
