4a1f7b038e70c6e3208944acf18dbdc76843e29b
MeteorClear <tivics9973@gmail.com>
2024-09-20 16:18:05 +0900

    feat: generating jwt token

--- src/main/java/com/example/markdownnotes/security/JwtTokenUtil.java
+++ src/main/java/com/example/markdownnotes/security/JwtTokenUtil.java
@@ -1,5 +1,30 @@
 package com.example.markdownnotes.security;
 
-public class JwtTokenUtil {
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.Date;
 
+public class JwtTokenUtil {
+	
+	@Value("${jwt.secret}")
+	private String SECRET_KEY;
+	
+	private static final long EXPIRATION_TIME = 86400000; // 1 day
+	
+	// 토큰 생성
+	public String generateToken(String username) {
+		return Jwts.builder()
+				.setSubject(username)
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+				.signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+				.compact();
+	}
+	
+	// 토큰 닉네임 파싱
+	
+	
 }
