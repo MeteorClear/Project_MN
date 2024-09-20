49724bdfd64e5f9db2311b0d8d5d0580542063dc
MeteorClear <tivics9973@gmail.com>
2024-09-20 16:41:45 +0900

    feat: basic filter setup

--- src/main/java/com/example/markdownnotes/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/markdownnotes/security/JwtAuthenticationFilter.java
@@ -1,5 +1,27 @@
 package com.example.markdownnotes.security;
 
-public class JwtAuthenticationFilter {
+import org.springframework.web.filter.OncePerRequestFilter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	private final JwtTokenUtil jwtTokenUtil;
+	
+	public JwtAuthenticationFilter(JwtTokenUtil jwtTokenUtil) {
+		this.jwtTokenUtil = jwtTokenUtil;
+	}
+	
+	// OncePerRequestFilter
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, 
+									HttpServletResponse response, 
+									FilterChain chain) 
+											throws ServletException, IOException {
+		// todo
+	}
 }
