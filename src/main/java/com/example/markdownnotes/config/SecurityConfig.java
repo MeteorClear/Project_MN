1ab550d5ac7878a78da8cf43e61de448c5b26fae
MeteorClear <tivics9973@gmail.com>
2024-09-20 17:42:21 +0900

    feat: password encoder

--- src/main/java/com/example/markdownnotes/config/SecurityConfig.java
+++ src/main/java/com/example/markdownnotes/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -47,6 +49,10 @@ public class SecurityConfig {
 	}
 	
 	// 비밀번호 인코딩
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	// 사용자 정보 반환
 }
