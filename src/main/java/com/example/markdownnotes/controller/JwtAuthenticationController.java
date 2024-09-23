a1ed2a6382a41f32392c333a6ac17550b5175994
MeteorClear <tivics9973@gmail.com>
2024-09-23 15:41:01 +0900

    feat: token creation

--- src/main/java/com/example/markdownnotes/controller/JwtAuthenticationController.java
+++ src/main/java/com/example/markdownnotes/controller/JwtAuthenticationController.java
@@ -1,11 +1,17 @@
 package com.example.markdownnotes.controller;
 
+import com.example.markdownnotes.model.JwtRequest;
+import com.example.markdownnotes.model.JwtResponse;
 import com.example.markdownnotes.security.JwtTokenUtil;
 import com.example.markdownnotes.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,14 +28,25 @@ public class JwtAuthenticationController {
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
 
+	// Jwt 토큰 발급
+	@PostMapping("/login")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+		
+		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+		
+		final UserDetails userDetails = customUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+		
+		final String token = jwtTokenUtil.generateToken(userDetails.getUsername());
+		
+		return ResponseEntity.ok(new JwtResponse(token));
+	}
+
 	// 사용자 인증
-	public void authenticate(String email, String password) throws Exception {
+	private void authenticate(String email, String password) throws Exception {
 		try {
 			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
 		} catch (BadCredentialsException e) {
 			throw new Exception("[ERROR]Invalid Credentials", e);
 		}
 	}
-	
-	// jwt 토큰 발급
 }
