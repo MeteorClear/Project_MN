87314402660508b8a980593af625d36786b00ae7
MeteorClear <tivics9973@gmail.com>
2024-09-23 15:17:25 +0900

    feat: basic setup

--- src/main/java/com/example/markdownnotes/controller/JwtAuthenticationController.java
+++ src/main/java/com/example/markdownnotes/controller/JwtAuthenticationController.java
@@ -1,5 +1,26 @@
 package com.example.markdownnotes.controller;
 
-public class JwtAuthenticationController {
+import com.example.markdownnotes.security.JwtTokenUtil;
+import com.example.markdownnotes.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/auth")
+public class JwtAuthenticationController {
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+	
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+
+	// 사용자 인증
+	
+	// jwt 토큰 발급
 }
