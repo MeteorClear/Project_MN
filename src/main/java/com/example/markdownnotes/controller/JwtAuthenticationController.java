80576ad6243310b01fb17115f63aba9cb0663a63
MeteorClear <tivics9973@gmail.com>
2024-09-23 15:33:35 +0900

    feat: user authentication

--- src/main/java/com/example/markdownnotes/controller/JwtAuthenticationController.java
+++ src/main/java/com/example/markdownnotes/controller/JwtAuthenticationController.java
@@ -4,6 +4,8 @@ import com.example.markdownnotes.security.JwtTokenUtil;
 import com.example.markdownnotes.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,13 @@ public class JwtAuthenticationController {
 	private CustomUserDetailsService customUserDetailsService;
 
 	// 사용자 인증
+	public void authenticate(String email, String password) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+		} catch (BadCredentialsException e) {
+			throw new Exception("[ERROR]Invalid Credentials", e);
+		}
+	}
 	
 	// jwt 토큰 발급
 }
