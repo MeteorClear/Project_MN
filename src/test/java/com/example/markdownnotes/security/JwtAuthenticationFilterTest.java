3202921b76d5b60884cc4922ac828baf49e95099
MeteorClear <tivics9973@gmail.com>
2024-09-25 16:59:10 +0900

    feat: basic setup

--- src/test/java/com/example/markdownnotes/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/example/markdownnotes/security/JwtAuthenticationFilterTest.java
@@ -1,5 +1,26 @@
 package com.example.markdownnotes.security;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class JwtAuthenticationFilterTest {
 
+	@InjectMocks
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	@Mock
+	private JwtTokenUtil jwtTokenUtil;
+	
+	private String username;
+	private String token;
+	
+	@BeforeEach
+	public void setup() {
+		username = "testuser";
+		token = "Bearer test_jwt_token";
+	}
 }
