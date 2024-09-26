fc2e4367cc551caf757e0668e2df617103190a55
MeteorClear <tivics9973@gmail.com>
2024-09-26 16:01:52 +0900

    feat: basic setup

--- src/test/java/com/example/markdownnotes/service/CustomUserDetailsServiceTest.java
+++ src/test/java/com/example/markdownnotes/service/CustomUserDetailsServiceTest.java
@@ -1,5 +1,33 @@
 package com.example.markdownnotes.service;
 
-public class CustomUserDetailsServiceTest {
+import com.example.markdownnotes.model.User;
+import com.example.markdownnotes.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomUserDetailsServiceTest {
+	
+	@Mock
+	private UserRepository userRepository;
+	
+	@InjectMocks
+	private CustomUserDetailsService customUserDetailsService;
+	
+	private User user;
+	
+	@BeforeEach
+	public void setup() {
+		user = new User();
+		user.setEmail("test@example.com");
+		user.setPassword("testpassword");
+	}
+	
+	// loadUserByUsername
 }
