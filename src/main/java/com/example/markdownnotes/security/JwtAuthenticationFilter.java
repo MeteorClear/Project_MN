7f5494e99a905c48ce82988a923a6cbaa8e347b0
MeteorClear <tivics9973@gmail.com>
2024-09-27 17:01:35 +0900

    fix: adding Component annotation

--- src/main/java/com/example/markdownnotes/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/markdownnotes/security/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.example.markdownnotes.security;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -11,6 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private final JwtTokenUtil jwtTokenUtil;
