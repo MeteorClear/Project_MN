94d2e690a8c0be40b405afc89e89f6468ed61412
MeteorClear <tivics9973@gmail.com>
2024-09-27 16:22:43 +0900

    fix: remove unused import

--- src/test/java/com/example/markdownnotes/controller/JwtAuthenticationControllerTest.java
+++ src/test/java/com/example/markdownnotes/controller/JwtAuthenticationControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
