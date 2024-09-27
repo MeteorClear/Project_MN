a7b306812142b6ef361a8a7ab213619ecc90a309
MeteorClear <tivics9973@gmail.com>
2024-09-27 17:38:44 +0900

    feat: basic setup

--- src/main/java/com/example/markdownnotes/config/SwaggerConfig.java
+++ src/main/java/com/example/markdownnotes/config/SwaggerConfig.java
@@ -1,8 +1,20 @@
 package com.example.markdownnotes.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class SwaggerConfig {
 
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Project MN API")
+						.version("0.0.1")
+						.description("API documentation for Project MN"));
+	}
 }
