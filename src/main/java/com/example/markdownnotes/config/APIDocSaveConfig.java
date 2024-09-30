5377009a36794c81afe5512005b597934b54f2b0
MeteorClear <tivics9973@gmail.com>
2024-09-30 16:15:32 +0900

    feat: save API doc

--- src/main/java/com/example/markdownnotes/config/APIDocSaveConfig.java
+++ src/main/java/com/example/markdownnotes/config/APIDocSaveConfig.java
@@ -1,5 +1,22 @@
 package com.example.markdownnotes.config;
 
-public class APIDocSaveConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import java.io.FileWriter;
+import java.io.IOException;
 
+@Configuration
+public class APIDocSaveConfig {
+	
+	@Autowired
+	private OpenAPI openAPI;
+	
+	public void saveOpenAPIDoc() {
+		try (FileWriter file = new FileWriter("openapi-doc.json")) {
+			file.write(openAPI.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
