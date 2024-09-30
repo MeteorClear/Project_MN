ec7639056276e4f0ea965601b91a60bbbeac7b3f
MeteorClear <tivics9973@gmail.com>
2024-09-30 19:18:42 +0900

    feat: adding security item components

--- src/main/java/com/example/markdownnotes/config/SwaggerConfig.java
+++ src/main/java/com/example/markdownnotes/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.example.markdownnotes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,6 +18,12 @@ public class SwaggerConfig {
 				.info(new Info()
 						.title("Project MN API")
 						.version("0.0.1")
-						.description("API documentation for Project MN"));
+						.description("API documentation for Project MN"))
+				.addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+				.components(new Components()
+						.addSecuritySchemes("bearerAuth", new SecurityScheme()
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT")));
 	}
 }
