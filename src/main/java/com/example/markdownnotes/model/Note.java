4d1d4c5b8619c14ca2538360add204dc8f9fd333
MeteorClear <tivics9973@gmail.com>
2024-10-03 15:41:29 +0900

    docs: add comments

--- src/main/java/com/example/markdownnotes/model/Note.java
+++ src/main/java/com/example/markdownnotes/model/Note.java
@@ -2,42 +2,63 @@ package com.example.markdownnotes.model;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 시스템의 메모 엔티티
+ * DB의 "Note" 테이블과 매핑
+ */
 @Entity
 @Table(name = "Note")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Getter
 @Setter
 public class Note {
-	// 테이블 정의
-	// DB 스키마에 맞출것
+
+	/**
+	 * 메모의 고유 식별자, 기본키
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	/**
+	 * 메모의 작성자, 외래키(User Table)
+	 */
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 	
+	/**
+	 * 메모의 제목
+	 */
 	@Column(nullable = false, length = 255)
 	private String title;
 	
+	/**
+	 * 메모의 내용
+	 */
 	@Column(nullable = false, columnDefinition = "TEXT")
 	private String content;
 	
+	/**
+	 * 메모의 생성일
+	 */
 	@Column(name = "created_at", nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
 	private LocalDateTime createdAt;
 	
+	/**
+	 * 메모의 수정일
+	 */
 	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
 	
-	// Constructor
+	/**
+	 * JPA를 위한 기본 생성자
+	 */
 	public Note() {
-		// To do
+		
 	}
 }
