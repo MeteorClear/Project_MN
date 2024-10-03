2d88d5a1837f3d0b05e2f52f9faa1c98b442a742
MeteorClear <tivics9973@gmail.com>
2024-10-03 16:42:09 +0900

    docs: add comments

--- src/main/java/com/example/markdownnotes/repository/NoteRepository.java
+++ src/main/java/com/example/markdownnotes/repository/NoteRepository.java
@@ -6,8 +6,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
+/**
+ * 메모(Note) 엔티티에 대한 데이터 접근 레이어 클래스.
+ * 
+ * JpaRepository를 상속받아 CRUD 기능을 제공.
+ */
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Integer> {
-	// 특정 사용자가 작성한 모든 메모 목록 조회
+	
+	/**
+	 * 특정 사용자가 작성한 모든 메모 조회.
+	 * 
+	 * @param user 메모를 작성한 사용자(User) 엔티티
+	 * @return 사용자에 의해 작성된 모든 메모(Note) 목록
+	 */
 	List<Note> findByUser(User user);
 }
--- src/main/java/com/example/markdownnotes/repository/UserRepository.java
+++ src/main/java/com/example/markdownnotes/repository/UserRepository.java
@@ -5,11 +5,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+/**
+ * 사용자(User) 엔티티에 대한 데이터 접근 레이어 클래스.
+ * 
+ * JpaRepository를 상속받아 CRUD 기능을 제공.
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-	// 이메일로 사용자 조회 - 로그인 용
+	
+	/**
+	 * 이메일을 통해 사용자를 조회
+	 * 
+	 * @param email 사용자의 이메일
+	 * @return 주어진 이메일에 해당하는 사용자
+	 */
 	Optional<User> findByEmail(String email);
 	
-	// 닉네임으로 사용자 조회 - 미정
+	/**
+	 * 닉네임을 통해 사용자를 조회
+	 * 
+	 * @param username 사용자의 닉네임
+	 * @return 주어진 닉네임에 해당하는 사용자
+	 */
 	Optional<User> findByUsername(String username);
 }
