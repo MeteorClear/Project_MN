d7af2ae6cd1ff7a351c6c806e1a99f3a731531d5
MeteorClear <tivics9973@gmail.com>
2024-10-04 17:07:05 +0900

    fix: remove unused feat

--- src/main/java/com/example/markdownnotes/controller/JwtAuthenticationController.java
+++ src/main/java/com/example/markdownnotes/controller/JwtAuthenticationController.java
@@ -10,7 +10,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,9 +27,6 @@ public class JwtAuthenticationController {
 	
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
 
 	// Jwt 토큰 발급
 	@PostMapping("/login")
