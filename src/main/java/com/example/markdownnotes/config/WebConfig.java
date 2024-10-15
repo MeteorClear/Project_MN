3ada3a0e057c9d2fd8e4b5f926465e358b481fc4
MeteorClear <tivics9973@gmail.com>
2024-10-15 18:46:04 +0900

    feat: set cors

--- src/main/java/com/example/markdownnotes/config/WebConfig.java
+++ src/main/java/com/example/markdownnotes/config/WebConfig.java
@@ -1,10 +1,23 @@
 package com.example.markdownnotes.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	@Value("${cors.allowed.origin}")
+	private String allowedOrigin;
 
 	// cors 설정
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins(allowedOrigin)
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedHeaders("*")
+			.allowCredentials(true);
+	}
 }
