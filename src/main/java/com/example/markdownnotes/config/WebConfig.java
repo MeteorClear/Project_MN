43d0f3db89587ffff33253a11f88160c3a2f99ca
MeteorClear <tivics9973@gmail.com>
2024-10-15 18:40:53 +0900

    add: WebMvcConfigurer

---
+++ src/main/java/com/example/markdownnotes/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.example.markdownnotes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	// cors 설정
+}
